cedd28a7e68c54c775161a9345110fc9702be9db
Yun Liu <yunliu0212@gmail.com>
2018-01-09 15:34:53 -0800

    heroku environment var set up

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser  = require('body-parser');
 const path  = require('path');
 const app = express();
-const PORT= process.env.PORT || 5000
+app.set('port', process.env.PORT || 3000)
 
 
 app.use(bodyParser.json());
@@ -12,6 +12,6 @@ app.get('/', function(req, res){
   res.send('hello team 7');
 });
 
-app.listen(PORT, ()=>{
-  console.log(`server listen on port ${PORT}`)
+app.listen(app.get('port'), function() {
+  console.log('server listen on port ' + app.get('port'))
 })
