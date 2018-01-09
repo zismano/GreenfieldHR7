e8229c8368edd5dd53664b1d575fd5dd4a09fc3b
Yun Liu <yunliu0212@gmail.com>
2018-01-09 14:19:06 -0800

    change back to nodemon

--- server/index.js
+++ server/index.js
@@ -9,11 +9,8 @@ app.use(bodyParser.json());
 //app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/', function(req, res){
-	console.log('haha')
-  res.send('hello world');
+  res.send('hello team 7');
 });
-
-
 
 app.listen(PORT, ()=>{
   console.log(`server listen on port ${PORT}`)
