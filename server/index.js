6591de273800687f1f93a7d911bea5d4bd80fade
Yun Liu <yunliu0212@gmail.com>
2018-01-08 20:45:21 -0800

    add endpoint in get method

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/', (req, res)=>{
-	res.status(200).json('hello world')
+app.get('/get', (req, res)=>{
+	console.log('lalal')
+	res.status(200).send('hello world')
 })
 
 app.listen(3000, ()=>{
