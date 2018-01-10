242fc549d9db998ab6d23e9c5bf208843cb966c7
Yun Liu <yunliu0212@gmail.com>
2018-01-09 19:09:10 -0800

    example

--- server/index.js
+++ server/index.js
@@ -9,10 +9,12 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/', function(req, res){
-	console.log('environment is ', process.env.GOOGLEMAP_KEY)
+	console.log('environment is ', process.env)
   res.send('hello team 7');
 });
 
 app.listen(app.get('port'), function() {
   console.log('server listen on port ' + app.get('port'))
 })
+
+// example
