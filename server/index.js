b53f4dcb9c412f26f9dcef859953fef2a0a9d6d6
Jimmy Tran <jimmyctran2013@gmail.com>
2018-01-10 16:54:08 -0800

    Implemented auth in server-side

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser  = require('body-parser');
 const path  = require('path');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy
 const app = express();
+
+passport.use(new googleStrategy({
+	clientID: 'PLACEHOLDERID',
+	clientSecret: 'PLACEHOLDERSECRET',
+	callbackURL: 'auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+	console.log(profile);
+}));
+
 app.set('port', process.env.PORT || 3000)
 
 
@@ -11,6 +22,12 @@ app.use(bodyParser.json());
 app.get('/', function(req, res){
   res.send('hello team 7');
 });
+
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['profile']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(app.get('port'), function() {
   console.log('server listen on port ' + app.get('port'))
