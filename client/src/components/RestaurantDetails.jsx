39b09c4ead8be8f90257f2657c379491a05665b8
Yun Liu <yunliu0212@gmail.com>
2018-01-11 15:54:45 -0800

    try ignore bundlejs

--- client/src/components/RestaurantDetails.jsx
+++ client/src/components/RestaurantDetails.jsx
@@ -9,14 +9,13 @@ class RestaurantDetails extends React.Component {
 
 	render() {
 		console.log(this.props.restaurant)
-		let rest = this.props.restaurant;
+		let restaurant = this.props.restaurant;
 		return(
 			<div className="restaurant-box">
 			<img alt="map" src={rest.map} />
 			<p>
 				{rest.name}
 				<br/>
-<<<<<<< HEAD
 				{restaurant.address}
 				<br/>
 				{restaurant.city}, {restaurant.state} {restaurant.zip}
@@ -30,22 +29,6 @@ class RestaurantDetails extends React.Component {
 				{restaurant.distance ? restaurant.distance : ''}
 			</p>
       </div>
-=======
-				{rest.address}
-				<br/>
-				{rest.city}, {rest.state} {rest.zip}
-				<br/>
-				{rest.phone}
-				<br/>
-				{rest.price}
-				<br/>
-				{rest.star}
-				<br/>
-				{rest.distance ? rest.distance : ''}
-			</p>
-
-            </div>
->>>>>>> integrate map and render restaurant list
 		)
 	}
 }
