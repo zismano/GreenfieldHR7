8ed4babc99391e927aa2d5392ba8243475d8fb98
Ofir Zisman <zismanofir@gmail.com>
2018-01-11 17:41:13 -0800

    Add fail test

--- test/test.js
+++ test/test.js
@@ -68,10 +68,9 @@ describe('Array', function() {
     });
   });
 
-  // describe('fail test', function() {
-  //   it('should return -1 when the value is not present', function(){
-  //     assert.equal(-1, [1,2,3,4].indexOf(4));
-  //   });  	
-  // })
-});
->>>>>>> Add test
+  describe('fail test', function() {
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3,4].indexOf(4));
+    });  	
+  })
+});>>>>>>> Add test
