e21af801799fe0a62282aa434d9f4355989cde03
Yun Liu <yunliu0212@gmail.com>
2018-01-12 13:13:08 -0800

    integrate map and render restaurant list

--- client/src/components/RestaurantDetails.jsx
+++ client/src/components/RestaurantDetails.jsx
@@ -9,6 +9,7 @@ class RestaurantDetails extends React.Component {
 
 	render() {
 		console.log(this.props.restaurant)
+<<<<<<< HEAD
 		let restaurant = this.props.restaurant;
 		return(
 			<div className="restaurant-box">
@@ -26,7 +27,45 @@ class RestaurantDetails extends React.Component {
 					<br/>
 					{restaurant.distance ? <div><b>Distance:</b> {restaurant.distance}</div> : ''}
 				</p>
+=======
+		let rest = this.props.restaurant;
+		return(
+			<div className="restaurant-box">
+			<img alt="map" src={rest.map} />
+			<p>
+				{rest.name}
+				<br/>
+<<<<<<< HEAD
+				{restaurant.address}
+				<br/>
+				{restaurant.city}, {restaurant.state} {restaurant.zip}
+				<br/>
+				{restaurant.phone}
+				<br/>
+				{restaurant.price}
+				<br/>
+				{restaurant.stars}
+				<br/>
+				{restaurant.distance ? restaurant.distance : ''}
+			</p>
+>>>>>>> integrate map and render restaurant list
       </div>
+=======
+				{rest.address}
+				<br/>
+				{rest.city}, {rest.state} {rest.zip}
+				<br/>
+				{rest.phone}
+				<br/>
+				{rest.price}
+				<br/>
+				{rest.star}
+				<br/>
+				{rest.distance ? rest.distance : ''}
+			</p>
+
+            </div>
+>>>>>>> integrate map and render restaurant list
 		)
 	}
 }
