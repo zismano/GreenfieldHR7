7d00670d111ca4287e996b5f1c7d284584e404b5
Ofir Zisman <zismanofir@gmail.com>
2018-01-11 18:21:52 -0800

    Add server tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const request = require('request');
 const expect = require('chai').expect;
 const pg = require('pg');
@@ -50,17 +49,9 @@ describe('server and database', function() {
   });
 });
 
-
-
-
-
-
-
-
-
-    
-=======
 var assert = require('assert');
+var express = require('express');
+var app = express();
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function(){
@@ -73,4 +64,59 @@ describe('Array', function() {
       assert.equal(3, [1,2,3,4].indexOf(4));
     });  	
   })
-});>>>>>>> Add test
+
+//const baseUrl = `http://localhost:${process.env.PORT}`;
+const baseUrl = 'http://127.0.0.1:3000';
+
+const request = require('request');
+const expect = require('chai').expect;
+const pg = require('pg');
+
+
+describe('server', function() {
+ it('should return the content of index.html', function(done) {
+   request('http://127.0.0.1:3000/', function(err, res, body) {
+       expect(res.statusCode).to.equal(200);
+       done();
+   })
+ });
+
+ it('should send back parsable stringified JSON when search by category', function(done) {
+   request('http://127.0.0.1:3000/restaurant/category/burger', function(err, res, body) {
+       expect(JSON.parse.bind(this, body)).to.not.throw();
+       done();
+   })
+ });
+
+ it('should send back an array when searchy by category', function(done) {
+   request('http://127.0.0.1:3000/restaurant/category/burger', function(error, response, body) {
+     var parsedBody = JSON.parse(body);
+     expect(parsedBody).to.be.an('array');
+     done();
+   });
+ });
+
+ it('should send back parsable stringified JSON when search by name', function(done) {
+   request('http://127.0.0.1:3000/restaurant/name/san', function(err, res, body) {
+       expect(JSON.parse.bind(this, body)).to.not.throw();
+       done();
+   })
+ });
+
+ it('should send back an array when search by name', function(done) {
+   request('http://127.0.0.1:3000/restaurant/name/san', function(error, response, body) {
+     var parsedBody = JSON.parse(body);
+     expect(parsedBody).to.be.an('array');
+     done();
+   });
+ });
+
+ it('Should 404 when asked for a nonexistent endpoint', function(done) {
+   request('http://127.0.0.1:3000/arglebargle', function(error, response, body) {
+     expect(response.statusCode).to.equal(404);
+     done();
+   });
+ });
+ 
+
+});
