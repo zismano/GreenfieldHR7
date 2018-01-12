0abfd59c210ee7c5b7ce0e09dbbf0bb7e4239f05
Ofir Zisman <zismanofir@gmail.com>
2018-01-11 17:41:13 -0800

    Add fail test

--- test/test.js
+++ test/test.js
@@ -74,11 +74,8 @@ const request = require('request');
 const expect = require('chai').expect;
 const pg = require('pg');
 const parse = require( 'utils-json-parse' );
-<<<<<<< HEAD
 
 const staticMap = require('../helpers/googleMaps.js');
-=======
->>>>>>> Add utils-json-parse package
 
 describe('server', function() {
  it('should return the content of index.html', function(done) {
@@ -89,11 +86,7 @@ describe('server', function() {
  });
 
  it('should send back parsable stringified JSON when search by category', function(done) {
-<<<<<<< HEAD
    request(`${baseUrl}/restaurant/category/burger`, function(err, res, body) {
-=======
-   request('http://127.0.0.1:3000/restaurant/category/burger', function(err, res, body) {
->>>>>>> Add utils-json-parse package
        expect(parse.bind(this, body)).to.not.throw();
        done();
    })
@@ -108,25 +101,12 @@ describe('server', function() {
  });
 
  it('should send back parsable stringified JSON when search by name', function(done) {
-<<<<<<< HEAD
    request(`${baseUrl}/restaurant/name/san`, function(err, res, body) {
-=======
-   request('http://127.0.0.1:3000/restaurant/name/san', function(err, res, body) {
->>>>>>> Add utils-json-parse package
        expect(parse.bind(this, body)).to.not.throw();
        done();
    })
  });
 
-<<<<<<< HEAD
- // it('should send back an array when search by name', function(done) {
- //   request(`${baseUrl}/restaurant/name/san`, function(error, response, body) {
- //     var parsedBody = parse(body);
- //     expect(parsedBody).to.be.an('array');
- //     done();
- //   });
- // });
-=======
  it('should send back an array when search by name', function(done) {
    request('http://127.0.0.1:3000/restaurant/name/san', function(error, response, body) {
      var parsedBody = parse(body);
@@ -134,7 +114,6 @@ describe('server', function() {
      done();
    });
  });
->>>>>>> Add utils-json-parse package
 
  it('Should 404 when asked for a nonexistent endpoint', function(done) {
    request(`${baseUrl}/arglebargle`, function(error, response, body) {
@@ -166,8 +145,3 @@ describe('google map', function() {
     done();
   })
 })
-  // describe('fail test', function() {
-  //   it('should return -1 when the value is not present', function(){
-  //     assert.equal(-1, [1,2,3,4].indexOf(4));
-  //   });  	
-  // })
