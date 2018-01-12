942470f0c14ac240cb5dbbb55e96200e6095785e
Ofir Zisman <zismanofir@gmail.com>
2018-01-11 17:43:44 -0800

    Add pass test

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ describe('Array', function() {
 
   describe('fail test', function() {
     it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3,4].indexOf(4));
+      assert.equal(3, [1,2,3,4].indexOf(4));
     });  	
   })
 });
