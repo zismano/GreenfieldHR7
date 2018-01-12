c9d5a1356a497479be8cd7f4c1a1fc6a8ddb5bc3
Ofir Zisman <zismanofir@gmail.com>
2018-01-11 20:58:28 -0800

    Add utils-json-parse package

--- test/test.js
+++ test/test.js
@@ -32,6 +32,7 @@ describe('server', function() {
  it('should return the content of index.html', function(done) {
    request(`${baseUrl}/`, function(err, res, body) {
 
+const parse = require( 'utils-json-parse' );
 
 describe('server', function() {
  it('should return the content of index.html', function(done) {
@@ -46,7 +47,7 @@ describe('server', function() {
        expect(parse.bind(this, body)).to.not.throw();
 
    request('http://127.0.0.1:3000/restaurant/category/burger', function(err, res, body) {
-       expect(JSON.parse.bind(this, body)).to.not.throw();
+       expect(parse.bind(this, body)).to.not.throw();
        done();
    })
  });
@@ -65,15 +66,15 @@ describe('server', function() {
 
  it('should send back an array when searchy by category', function(done) {
    request('http://127.0.0.1:3000/restaurant/category/burger', function(error, response, body) {
- //    var parsedBody = JSON.parse(body);
-     expect(body).to.be.an('string');
+     var parseBody = parse(body);
+     expect(parseBody).to.be.an('array');
      done();
    });
  });
 
  it('should send back parsable stringified JSON when search by name', function(done) {
    request('http://127.0.0.1:3000/restaurant/name/san', function(err, res, body) {
-       expect(JSON.parse.bind(this, body)).to.not.throw();
+       expect(parse.bind(this, body)).to.not.throw();
        done();
    })
  });
@@ -91,7 +92,7 @@ describe('server', function() {
 
  it('should send back an array when search by name', function(done) {
    request('http://127.0.0.1:3000/restaurant/name/san', function(error, response, body) {
-     var parsedBody = JSON.parse(body);
+     var parsedBody = parse(body);
      expect(parsedBody).to.be.an('array');
      done();
    });
