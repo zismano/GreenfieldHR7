f12ecd9559525ac612aad7ed69ef1adabde02ac7
Ofir Zisman <zismanofir@gmail.com>
2018-01-11 17:37:57 -0800

    Add test

---
+++ test/test.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+
+  // describe('fail test', function() {
+  //   it('should return -1 when the value is not present', function(){
+  //     assert.equal(-1, [1,2,3,4].indexOf(4));
+  //   });  	
+  // })
+});
