8689cb328747b7c04f24f2c860c65abe9b0e5f9e
Ofir Zisman <zismanofir@gmail.com>
2018-01-11 19:43:10 -0800

    Check babel es6

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ describe('Array', function() {
     });
   });
 
-  describe('fail test', function() {
+  describe('fail test', () => {
     it('should return -1 when the value is not present', function(){
       assert.equal(3, [1,2,3,4].indexOf(4));
     });  	
