a9f66bb3675b7a21bffc57c086683a73fd7aec0c
Ofir Zisman <zismanofir@gmail.com>
2018-01-11 16:33:38 -0800

    Add script

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleMapsStaticKey: 'AIzaSyBVkQ7R8J8U_hHVL346Sv7LiAeICgEA9kU',
+  googleClientCallBackURL: 'http://127.0.0.1:3000/auth/google/callback',
+  googleGeolocationKey: 'AIzaSyC-Kk055PHzGM9lMzbwDQIk7gjdaQn1Pnw',
+  cookieKey: 'vanvsklslckv',
+  googleClientId: '754440988413-k24o4pr9oj39k18lkiu2k05gpq1551qo.apps.googleusercontent.com',
+  googleClientSecret: 'eciCuEJWfN3y74il4npnF83W'
+};
