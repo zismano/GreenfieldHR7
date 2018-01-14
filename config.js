0d539bc7aa9bbb1db1fc927f973d83a44a8e8c5d
Yun Liu <yunliu0212@gmail.com>
2018-01-13 16:48:57 -0800

    test config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  googleMapsStaticKey: 'AIzaSyBVkQ7R8J8U_hHVL346Sv7LiAeICgEA9kU',
+  googleMapsStaticKey: 'AIzaSyCPSufCrG5PjcFGv2f7ZBwKQCNXNc9-H9k',
   googleClientCallBackURL: 'http://127.0.0.1:3000/auth/google/callback',
-  googleGeolocationKey: 'AIzaSyC-Kk055PHzGM9lMzbwDQIk7gjdaQn1Pnw',
-  cookieKey: 'vanvsklslckv',
-  googleClientID: '754440988413-k24o4pr9oj39k18lkiu2k05gpq1551qo.apps.googleusercontent.com',
-  googleClientSecret: 'eciCuEJWfN3y74il4npnF83W'
+  googleGeolocationKey: 'AIzaSyBjMaf2WBv5hb2-_TKjN300wAYDT8yvoXM',
+  cookieKey: '',
+  googleClientID: '',
+  googleClientSecret: '',
+  googlePlacesKey: 'AIzaSyDDDSHDTUDelicZ1gIWMvo3x9H9tUVFf7M'
 };
