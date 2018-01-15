a526080806cf9611125b54234dfec311c898fa23
Jimmy Tran <jimmyctran2013@gmail.com>
2018-01-15 10:05:40 -0800

    Re-added addReview function lost due to merge

--- database/index.js
+++ database/index.js
@@ -143,6 +143,17 @@ let bookmarkRestaurant = (userId, restaurantId, callback) => {
 	})
 }
 
+let addReview = (review, callback) => {
+	const insertQuery = 'insert into reviews (user_id, restaurant_id, createddate, star, comment) values ($1, $2, localtimestamp, $3, $4)';
+	client.query(insertQuery, [review.userId, review.restaurantId, review.reviewStars, review.reviewComment], (err, res) => {
+		if (err) {
+			callback(err.stack, null);
+		} else {
+			callback(null, res.rows[0]);
+		}
+	})
+}
+
 module.exports = {
   searchByRestaurantName: searchByRestaurantName,
   searchByRestaurantCategory: searchByRestaurantCategory,
